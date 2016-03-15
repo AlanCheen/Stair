10bd5b9b4ac43ae7faae9cbad580f08687d7644d
czn <2985697630@qq.com>
2016-03-15 15:31:44 +0800

    新增logger

---
+++ app/src/main/java/yfy/github/stair/utils/Logger.java
@@ -0,0 +1,7 @@
+package yfy.github.stair.utils;
+
+/**
+ * Created by 程序亦非猿 on 16/3/15.
+ */
+public interface Logger {
+}
---
+++ app/src/main/java/yfy/github/stair/utils/ZLog.java
@@ -0,0 +1,29 @@
+package yfy.github.stair.utils;
+
+/**
+ * Created by 程序亦非猿 on 16/3/15.
+ */
+public class ZLog implements Logger{
+
+
+    @Override
+    public void v(String tag, String msg) {
+
+    }
+
+    @Override
+    public void d(String tag, String msg) {
+
+    }
+
+    @Override
+    public void i(String tag, String msg) {
+
+    }
+
+    @Override
+    public void e(String tag, String msg) {
+
+    }
+
+}
